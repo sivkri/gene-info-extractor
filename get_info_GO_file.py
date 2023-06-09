fb8bcafec3d4c327163a1093c78c46892319f45f
Sivakumar Krishnamoorthy <indiangene@gmail.com>
2023-06-09 13:37:04 +0200

    Update get_info_GO_file.py

--- get_info_GO_file.py
+++ get_info_GO_file.py
@@ -1,41 +1,41 @@
-# usage: python3 get_info.py info_BP.csv gene_ID.csv output.csv
+# usage: python3 get_info.py info_BP.csv gene_ID.csv output.csv       
 import sys
 import csv
 
-info_source=sys.argv[1]  # info_BP.csv
-query_list= sys.argv[2]  # gene_ID.csv
-out_file_name =  sys.argv[3]  # output.csv
-
-#print(info_source,query_list)
-
 # Transforming the given csv information into dictionary
-info_dict={}
-
-with open(info_source, 'r') as source:
-    next(source)
-    source_reader = csv.reader(source)
-    for info_row in source_reader:
-        info_gene_ids=info_row[1].split()  # Defense response, [AT1G12220 AT1G14780 AT1G48500 AT1G66100]
-        #print (info_row[0],info_gene_ids[0])
-        for gene in info_gene_ids:
-            if gene in info_dict:
-                info_dict[gene].append(info_row[0])
-            else:
-                info_dict[gene]=[info_row[0]]
-        
-#print(info_dict)
-
-out_file = open(out_file_name, 'w')
-out_file_err = open(out_file_name+"_no_info.csv", 'w')
+def transform_csv_to_dict(info_source):
+    info_dict = {}
+    with open(info_source, 'r') as source:
+        next(source)
+        source_reader = csv.reader(source)
+        for info_row in source_reader:
+            info_gene_ids = info_row[1].split()
+            for gene in info_gene_ids:
+                info_dict.setdefault(gene, []).append(info_row[0])
+    return info_dict
 
 # Appending the information from dictionary to input gene_ID
 
-with open(query_list,'r') as query:
-    next(query)
-    query_reader=csv.reader(query)
-    for query_row in query_reader:
-        if query_row[0] in info_dict:
-            unique_annotation = list(set(info_dict[query_row[0]]))
-            out_file.write(query_row[0]+","+",".join(map(str, unique_annotation))+"\n")
-        else:
-            out_file_err.write(query_row[0]+"\n")
+def process_query(info_dict, query_list, out_file_name):
+    with open(query_list, 'r') as query, open(out_file_name, 'w') as out_file, open(out_file_name+"_no_info.csv", 'w') as out_file_err:
+        next(query)
+        query_reader = csv.reader(query)
+        for query_row in query_reader:
+            gene_id = query_row[0]
+            if gene_id in info_dict:
+                unique_annotation = set(info_dict[gene_id])
+                out_file.write(gene_id + "," + ",".join(unique_annotation) + "\n")
+            else:
+                out_file_err.write(gene_id + "\n")
+
+if __name__ == "__main__":
+    if len(sys.argv) < 4:
+        print("Usage: python3 get_info.py info_BP.csv gene_ID.csv output.csv")
+        sys.exit(1)
+
+    info_source=sys.argv[1]  # info_BP.csv
+    query_list= sys.argv[2]  # gene_ID.csv
+    out_file_name =  sys.argv[3]  # output.csv
+
+    info_dict = transform_csv_to_dict(info_source)
+    process_query(info_dict, query_list, out_file_name)
